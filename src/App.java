ccb96c4d132585b93be1e9b9a60c4989e6165b33
Atila jr <atilla.junior33@gmail.com>
2023-01-06 12:39:44 -0300

    tipos curinga pode fazer métodos que recebem um genérico de qualquer tipo!

--- src/App.java
+++ src/App.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class App {
@@ -6,9 +6,23 @@ public class App {
   public static void main(String[] args) throws Exception {
     System.out.println("==============================");
 
-    // O supertipo de qualquer tipo de lista é List<?>. Este é um tipo curinga:
-    List<?> myObjs = new ArrayList<Object>();
-    List<Integer> myNumbers = new ArrayList<Integer>();
-    myObjs = myNumbers; // erro de compilação
+    // Com tipos curinga podemos fazer métodos que recebem um genérico de "qualquer tipo":
+
+    List<Integer> myInts = Arrays.asList(5, 2, 10);
+    printList(myInts);
+
+    List<String> myStrs = Arrays.asList("Maria", "Alex", "Bob");
+    printList(myStrs);
+  }
+
+  // esse metodo aki funcionaria para qualquer outro tipo de lista repare que
+  // acima eu tenho 2 declaraçao de list diferente
+
+  public static void printList(List<?> list) {
+    for (Object obj : list) {
+      System.out.println(obj);
+
+      System.out.println("=============Fim================");
+    }
   }
 }
